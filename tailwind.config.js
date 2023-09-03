ca9b5bb99c3b6f84a00542362344ba46927bbcf5
Aditya mishra <42762281+iadii@users.noreply.github.com>
2023-09-04 04:47:06 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./dist/index.html"],
+  content: ["./dist/*.html"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'jel':'#0284c7',
+      'corn':'#0d9488',
+      'vase':'#166534',
+      'fav':'#334155',
+      'tail':'#fefce8'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
